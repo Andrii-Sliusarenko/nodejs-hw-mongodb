c4b0b12d5c7b64232592734179446c070fd114a0
Andrii <andrii.sliusarenko@gmail.com>
2024-12-13 13:38:54 +0200

    add controllers

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,1 +1,23 @@
-[];
+import { getAllContacts, getContactsById } from '../services/contacts.js';
+
+export const getAllContactsController = async (req, res, next) => {
+  const data = await getAllContacts();
+  res.json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data,
+  });
+};
+
+export const getAllContactsByIdController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const data = await getContactsById(contactId);
+  if (!data) {
+    return res.status(404).json({ message: 'Contact not found' });
+  }
+  res.json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data,
+  });
+};
