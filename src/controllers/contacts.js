04f46cf89270fa7ee8be2684a1cca8d35dfb964b
Andrii <andrii.sliusarenko@gmail.com>
2024-12-23 12:54:37 +0200

    add filter hw4 complete

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -4,15 +4,18 @@ import createHttpError from 'http-errors';
 
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 
 export const getAllContactsController = async (req, res) => {
   const { page, perPage } = parsePaginationParams(req.query);
   const { sortBy, sortOrder } = parseSortParams(req.query);
+  const filter = parseFilterParams(req.query);
   const contacts = await contactsServices.getAllContacts({
     page,
     perPage,
     sortBy,
     sortOrder,
+    filter,
   });
   res.json({
     status: 200,
