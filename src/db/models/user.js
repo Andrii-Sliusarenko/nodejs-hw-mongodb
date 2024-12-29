187f42e4be598d8983c9c0369c13c9a7e3d52613
Andrii <andrii.sliusarenko@gmail.com>
2024-12-29 15:49:02 +0200

    add login and cookies

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,6 +1,5 @@
 import { model, Schema } from 'mongoose';
 import { mailFormat } from '../../constants/index.js';
-import { handleSaveError } from './hooks.js';
 
 const userSchema = new Schema(
   {
@@ -16,12 +15,16 @@ const userSchema = new Schema(
     },
     password: {
       type: String,
+      minlength: 6,
       required: true,
     },
   },
   { versionKey: false, timestamps: true },
 );
-
-userSchema.post('save', handleSaveError);
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
 
 export const UsersCollection = model('users', userSchema);
--- src/validation/auth.js
+++ src/validation/auth.js
@@ -3,5 +3,9 @@ import { mailFormat } from '../constants/index.js';
 export const registerUserSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().pattern(mailFormat).required(),
-  password: Joi.string().required(),
+  password: Joi.string().min(6).required(),
+});
+export const loginUserSchema = Joi.object({
+  email: Joi.string().pattern(mailFormat).required(),
+  password: Joi.string().min(6).required(),
 });
